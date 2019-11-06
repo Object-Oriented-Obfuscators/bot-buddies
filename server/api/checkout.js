625796065327ad414eaa6025adcd0a91c460e237
lester.dominguez@gmail.com <Lester.dominguez@gmail.com>
2019-11-06 15:06:19 -0500

    Put route working but half complete 2

--- server/api/checkout.js
+++ server/api/checkout.js
@@ -2,17 +2,23 @@ const router = require('express').Router()
 const {Carts, CartsProducts, Products} = require('../db/models/index')
 
 router.put('/', async (req, res, next) => {
+  let total = 0
   try {
-    await Carts.update(
-      {
-        total: 509
-      },
-      {
-        where: {
-          id: req.session.cartId
-        }
-      }
-    ).then(res.sendStatus(200))
+    const cart = await Carts.findByPk(req.session.cartId, {
+      include: [{model: Products}]
+    })
+
+    // await Carts.update(
+    //   {
+    //     total: 509,
+    //     date: new Date()
+    //   },
+    //   {
+    //     where: {
+    //       id: req.session.cartId
+    //     }
+    //   }
+    // ).then(res.sendStatus(200))
   } catch (error) {
     next(error)
   }
