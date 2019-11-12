133e7a6b26f00c90f631f281fd42ff08f7594270
lester.dominguez@gmail.com <Lester.dominguez@gmail.com>
2019-11-12 11:37:47 -0500

    Configured Travis setting

--- server/api/index.js
+++ server/api/index.js
@@ -3,7 +3,6 @@ const router = require('express').Router()
 // router.use('/users', require('./users'))
 router.use('/products', require('./products'))
 router.use('/cart', require('./cart'))
-router.use('/checkout', require('./checkout'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
